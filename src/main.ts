f8153a433a3d02129425a863161bc1c278fd942e
Jing Han, Huang <7jesstc1999@gmail.com>
2024-07-17 19:07:54 +0800

    refactor: import primevue component in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import PrimeVue from 'primevue/config';
+import Noir from './presets/Noir.js';
+import Menubar from 'primevue/menubar';
+import SelectButton from 'primevue/selectbutton';
 
 // PrimeVue CSS
 import 'primeicons/primeicons.css';                          // Icons
@@ -8,6 +11,18 @@ import 'primeflex/primeflex.css';                            // PrimeFlex
 
 const app = createApp(App);
 
-app.use(PrimeVue);
+app.use(PrimeVue, {
+    theme: {
+        preset: Noir,
+        options: {
+            prefix: 'p',
+            darkModeSelector: '.p-dark',
+            cssLayer: false,
+        }
+    }
+});
+
+app.component('MenuBar', Menubar);
+app.component('SelectButton', SelectButton);
 
 app.mount('#app');
