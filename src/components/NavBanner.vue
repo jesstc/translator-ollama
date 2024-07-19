b8e3deb279c754dbd8a615dca798b6c8bd6c240d
Jing Han, Huang <7jesstc1999@gmail.com>
2024-07-19 15:57:11 +0800

    style: update NavBanner height

--- src/components/NavBanner.vue
+++ src/components/NavBanner.vue
@@ -1,7 +1,7 @@
   <template>
     <Menubar class="surface-overlay p-3 shadow-2 flex align-items-center justify-content-between relative lg:static">
       <template #start>
-        <img height="40" viewBox="0 0 35 40" fill="none" src="@/assets/logo.png" alt="Logo" class="logo" />
+        <img height="30" viewBox="0 0 35 40" fill="none" src="@/assets/logo.png" alt="Logo" class="logo" />
       </template>
     </Menubar>
   </template>
